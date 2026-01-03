09fedfac3009d7aa566ff66f788d9f1003addee8
aditya soni <dev.aditya270325@gmail.com>
2026-01-03 15:04:14 +0530

    initial commit

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb://localhost:27017/';
+
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectToMongo;
